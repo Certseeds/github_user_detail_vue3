8ab71f57b0e30405e52caa6e3c166a940e7b9c00
Certseeds <51754303+Certseeds@users.noreply.github.com>
2021-01-02 11:13:30 +0800

    Branch: master, add way of declare type in return type.
    Signed-off-by: Killer_Quinn <51754303+Certseeds@users.noreply.github.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,7 @@
  * @Author: nanoseeds
  * @Date: 2021-01-02 00:23:17
  * @LastEditors: nanoseeds
- * @LastEditTime: 2021-01-02 01:24:53
+ * @LastEditTime: 2021-01-02 11:12:40
 -->
 <template>
   <div class="hello">
@@ -14,7 +14,6 @@
 
 <script lang="ts">
 import { defineComponent, PropType } from "vue";
-
 const Helloworld = defineComponent({
   props: {
     success: { type: String },
@@ -22,14 +21,19 @@ const Helloworld = defineComponent({
       type: Function as PropType<() => void>,
     },
   },
-  data() {
+  data(): {
+    msg: string;
+    number1: number;
+  } {
     return {
-      msg: "Vue3 code style",
+      msg: "vue3 code Style",
+      number1: 2,
     };
   },
   methods: {
     reversedMessage(): string {
       this.msg = this.msg.split(" ").reverse().join(" ");
+      this.number1 += 1;
       return this.msg;
     },
   },
